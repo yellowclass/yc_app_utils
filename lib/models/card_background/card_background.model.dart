7344e0c9ebf8055cb2effb1a6695771e564d0484
vanshg395 <vansh@ivypods.com>
2022-06-20 22:29:12 +0530

    card bg added

---
+++ lib/models/card_background/card_background.model.dart
@@ -0,0 +1,52 @@
+import 'package:yc_app_utils/helpers/common_helpers.dart';
+import 'package:yc_app_utils/models/section_background/section_background_direction.enum.dart';
+import 'package:yc_app_utils/models/section_background/section_bg_type.enum.dart';
+
+class CardBackground {
+  CardBackground({
+    this.backgroundType,
+    this.backgroundColor,
+    this.backgroundImageUrl,
+    this.aspectRatio,
+    this.gradientColor1,
+    this.gradientColor2,
+    this.gradientDirection,
+    this.shouldLeaveBorder = false,
+  });
+
+  SectionBgType? backgroundType;
+  String? backgroundColor;
+  String? backgroundImageUrl;
+  String? aspectRatio;
+  String? gradientColor1;
+  String? gradientColor2;
+  SectionGradientDirections? gradientDirection;
+  bool shouldLeaveBorder;
+
+  factory CardBackground.fromJson(Map<String, dynamic> json) => CardBackground(
+        backgroundType: CommonHelpers.enumFromString(
+              SectionBgType.values,
+              json["backgroundType"],
+            ) ??
+            SectionBgType.TRANSPARENT,
+        backgroundColor: json["backgroundColor"],
+        backgroundImageUrl: json["backgroundImageUrl"],
+        aspectRatio: json["aspectRatio"],
+        gradientColor1: json["gradientColor1"],
+        gradientColor2: json["gradientColor2"],
+        gradientDirection: CommonHelpers.enumFromString(
+            SectionGradientDirections.values, json["gradientDirection"]),
+        shouldLeaveBorder: json["shouldLeaveBorder"] ?? false,
+      );
+
+  Map<String, dynamic> toJson() => {
+        "backgroundType": backgroundType?.name,
+        "backgroundColor": backgroundColor,
+        "backgroundImageUrl": backgroundImageUrl,
+        "aspectRatio": aspectRatio,
+        "gradientColor1": gradientColor1,
+        "gradientColor2": gradientColor2,
+        "gradientDirection": gradientDirection?.name,
+        "shouldLeaveBorder": shouldLeaveBorder,
+      };
+}
