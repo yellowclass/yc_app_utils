a4c253fe7e04ad257ad1b8c1cab7ef3486a6518f
vanshg395 <vansh@ivypods.com>
2022-06-21 13:08:02 +0530

    import simplified

--- lib/app_utils.dart
+++ lib/app_utils.dart
@@ -1,1 +1,4 @@
 library app_utils;
+
+export 'package:yc_app_utils/ui/ui.dart';
+export 'package:yc_app_utils/models/models.dart';
---
+++ lib/models/models.dart
@@ -0,0 +1,8 @@
+// card background
+export 'package:yc_app_utils/models/card_background/card_background.model.dart';
+
+// section background
+export 'package:yc_app_utils/models/section_background/radial_gradient.model.dart';
+export 'package:yc_app_utils/models/section_background/section_background.model.dart';
+export 'package:yc_app_utils/models/section_background/section_background_direction.enum.dart';
+export 'package:yc_app_utils/models/section_background/section_bg_type.enum.dart';
