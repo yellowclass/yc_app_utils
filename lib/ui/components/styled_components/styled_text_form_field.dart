04d7f862fdc3857e299b41406dd0b1118fa47767
vanshg395 <vansh@ivypods.com>
2022-06-23 12:07:44 +0530

    chaneg

--- lib/ui/components/styled_components/styled_text_form_field.dart
+++ lib/ui/components/styled_components/styled_text_form_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yc_app_utils/ui/components/styled_components/v2_styled_text.widget.dart';
 import 'package:yc_app_utils/yc_app_utils.dart';
 
 class StyledTextFormField extends StatelessWidget {
@@ -14,6 +15,10 @@ class StyledTextFormField extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
+        if (formFieldData.label != null)
+          V2StyledTextWidget(
+            styledText: formFieldData.label!,
+          ),
         TextFormField(),
       ],
     );
