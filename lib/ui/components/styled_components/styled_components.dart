bcdcea631eace493ff2ea2563e722a1c5cf7593a
vanshg395 <vansh@ivypods.com>
2022-06-28 18:50:50 +0530

    video dummy component

--- lib/ui/components/styled_components/styled_components.dart
+++ lib/ui/components/styled_components/styled_components.dart
@@ -1,3 +1,4 @@
 export 'package:yc_app_utils/ui/components/styled_components/styled_component.widget.dart';
 export 'package:yc_app_utils/ui/components/styled_components/v2_styled_text.widget.dart';
 export 'package:yc_app_utils/ui/components/styled_components/styled_image.widget.dart';
+export 'package:yc_app_utils/ui/components/styled_components/styled_video.widget.dart';
