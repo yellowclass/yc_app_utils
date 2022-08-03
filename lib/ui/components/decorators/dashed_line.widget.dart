972654c28717686c7a3fffb16b833b7d38a07edb
soninaman <naman@ivypods.com>
2022-08-03 12:37:45 +0530

    Refactored

--- lib/ui/components/decorators/dashed_line.widget.dart
+++ lib/ui/components/decorators/dashed_line.widget.dart
@@ -1,29 +1,29 @@
 import 'package:flutter/material.dart';
 
+import 'package:yc_app_utils/ui/styleguide/colors.dart';
+
 class DashedLine extends StatelessWidget {
   const DashedLine({
     Key? key,
-    this.height = 1,
-    this.color = Colors.black,
-    this.width = 5.0,
+    this.thickness = 1,
+    this.color = AppColors.cBLACK,
+    this.dashWidth = 5.0,
   }) : super(key: key);
-  final double height;
+  final double thickness;
   final Color color;
-  final double width;
+  final double dashWidth;
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (BuildContext context, BoxConstraints constraints) {
-        final boxWidth = constraints.constrainWidth();
-        final dashWidth = width;
-        final dashHeight = height;
-        final dashCount = (boxWidth / (2 * dashWidth)).floor();
+        final _boxWidth = constraints.constrainWidth();
+        final _dashCount = (_boxWidth / (2 * dashWidth)).floor();
         return Flex(
-          children: List.generate(dashCount, (_) {
+          children: List.generate(_dashCount, (_) {
             return SizedBox(
               width: dashWidth,
-              height: dashHeight,
+              height: thickness,
               child: DecoratedBox(
                 decoration: BoxDecoration(color: color),
               ),
