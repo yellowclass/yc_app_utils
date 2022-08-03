bc467d3fc9b73fef4fc54e1ec578b08618de1d18
soninaman <naman@ivypods.com>
2022-08-03 13:01:53 +0530

    Refactored

--- lib/ui/components/decorators/dashed_line.widget.dart
+++ lib/ui/components/decorators/dashed_line.widget.dart
@@ -8,31 +8,39 @@ class DashedLine extends StatelessWidget {
     this.thickness = 1,
     this.color = AppColors.cBLACK,
     this.dashWidth = 5.0,
+    this.width = double.infinity,
   }) : super(key: key);
   final double thickness;
   final Color color;
   final double dashWidth;
+  final double width;
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        final _boxWidth = constraints.constrainWidth();
-        final _dashCount = (_boxWidth / (2 * dashWidth)).floor();
-        return Flex(
-          children: List.generate(_dashCount, (_) {
-            return SizedBox(
-              width: dashWidth,
-              height: thickness,
-              child: DecoratedBox(
-                decoration: BoxDecoration(color: color),
-              ),
-            );
-          }),
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          direction: Axis.horizontal,
-        );
-      },
+    return SizedBox(
+      width: width,
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          final _boxWidth = constraints.constrainWidth();
+          final _dashCount = (_boxWidth / (2 * dashWidth)).floor();
+          return Flex(
+            children: List.generate(
+              _dashCount,
+              (_) {
+                return SizedBox(
+                  width: dashWidth,
+                  height: thickness,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(color: color),
+                  ),
+                );
+              },
+            ),
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            direction: Axis.horizontal,
+          );
+        },
+      ),
     );
   }
 }
