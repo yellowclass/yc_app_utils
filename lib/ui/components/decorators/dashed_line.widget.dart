af9712db3b201c8313822e2972f408769f0cb80a
soninaman <naman@ivypods.com>
2022-09-20 13:38:46 +0530

    vertical support for dashed line

--- lib/ui/components/decorators/dashed_line.widget.dart
+++ lib/ui/components/decorators/dashed_line.widget.dart
@@ -9,38 +9,66 @@ class DashedLine extends StatelessWidget {
     this.color = AppColors.cBLACK,
     this.dashWidth = 5.0,
     this.width = double.infinity,
+    this.axis = Axis.horizontal,
   }) : super(key: key);
   final double thickness;
   final Color color;
   final double dashWidth;
   final double width;
+  final Axis axis;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: width,
-      child: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          final _boxWidth = constraints.constrainWidth();
-          final _dashCount = (_boxWidth / (2 * dashWidth)).floor();
-          return Flex(
-            children: List.generate(
-              _dashCount,
-              (_) {
-                return SizedBox(
-                  width: dashWidth,
-                  height: thickness,
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(color: color),
+    return axis == Axis.horizontal
+        ? SizedBox(
+            width: width,
+            child: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                final _boxWidth = constraints.constrainWidth();
+                final _dashCount = (_boxWidth / (2 * dashWidth)).floor();
+                return Flex(
+                  children: List.generate(
+                    _dashCount,
+                    (_) {
+                      return SizedBox(
+                        width: dashWidth,
+                        height: thickness,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(color: color),
+                        ),
+                      );
+                    },
                   ),
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  direction: Axis.horizontal,
                 );
               },
             ),
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            direction: Axis.horizontal,
+          )
+        : SizedBox(
+            height: width,
+            child: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                final _boxHeight = constraints.constrainHeight();
+                final _dashCount = (_boxHeight / (2 * dashWidth)).floor();
+                return Flex(
+                  children: List.generate(
+                    _dashCount,
+                    (_) {
+                      return SizedBox(
+                        height: dashWidth,
+                        width: thickness,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(color: color),
+                        ),
+                      );
+                    },
+                  ),
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  direction: Axis.vertical,
+                );
+              },
+            ),
           );
-        },
-      ),
-    );
   }
 }
