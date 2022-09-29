c28555291e102bee7abc4386c86668ad93f441d7
kshivam1177 <shivam@ivypods.com>
2022-09-29 11:59:25 +0530

    Work in Progress

--- lib/ui/components/image_viewer/image_viewer.dart
+++ lib/ui/components/image_viewer/image_viewer.dart
@@ -1,30 +1,23 @@
-import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
 import 'package:yc_app_utils/yc_app_utils.dart';
 
 class ImageViewer extends StatelessWidget {
-  const ImageViewer({
-    required this.images,
-    this.defaultSelectedIndex = 0,
-    this.defaultImageKey,
-    Key? key,
-  }) : super(key: key);
-
   final List<String> images;
   final int defaultSelectedIndex;
   final String? defaultImageKey;
+  final ValueChanged<String>? onDownloadClick;
+  late int _currentIndex = 0;
 
-  Widget buildChild(int i, String imgUrl) {
-    Widget img = CachedNetworkImage(
-      imageUrl: imgUrl,
-      fit: BoxFit.contain,
-    );
-    if (i == defaultSelectedIndex && defaultImageKey != null) {
-      return Hero(tag: defaultImageKey!, child: img);
-    } else {
-      return img;
-    }
+  ImageViewer({
+    required this.images,
+    this.defaultSelectedIndex = 0,
+    this.defaultImageKey,
+    this.onDownloadClick,
+    Key? key,
+  }) : super(key: key) {
+    _currentIndex = defaultSelectedIndex;
   }
 
   @override
@@ -37,24 +30,39 @@ class ImageViewer extends StatelessWidget {
           color: AppColors.cBLACK,
         ),
       ),
+      floatingActionButton: onDownloadClick == null
+          ? null
+          : FloatingActionButton(
+              mini: true,
+              child: const Icon(Icons.download_outlined),
+              onPressed: () => onDownloadClick!.call(images[_currentIndex]),
+            ),
       body: SizedBox(
         height: double.infinity,
         child: CarouselSlider(
           options: CarouselOptions(
-            initialPage: defaultSelectedIndex,
-            height: double.infinity,
-            enableInfiniteScroll: false,
-            viewportFraction: 1,
-          ),
-          items: [
-            for (var i = 0; i < images.length; i++)
-              buildChild(
-                i,
-                images[i],
-              ),
-          ],
+              initialPage: defaultSelectedIndex,
+              height: double.infinity,
+              enableInfiniteScroll: false,
+              viewportFraction: 1,
+              onPageChanged: (index, changeReason) {
+                _currentIndex = index;
+              }),
+          items: List<Widget>.generate(images.length, buildChild),
         ),
       ),
     );
+  }
+
+  Widget buildChild(int index) {
+    Widget img = CachedNetworkImage(
+      imageUrl: images[index],
+      fit: BoxFit.contain,
+    );
+    if (index == defaultSelectedIndex && defaultImageKey != null) {
+      return Hero(tag: defaultImageKey!, child: img);
+    } else {
+      return img;
+    }
   }
 }
