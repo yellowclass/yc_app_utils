429d172366cce8374b02c521646f7f28cf360798
vanshg395 <vansh@ivypods.com>
2022-11-08 16:36:13 +0530

    styled img fix

--- lib/ui/components/styled_components/styled_image.widget.dart
+++ lib/ui/components/styled_components/styled_image.widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shimmer/shimmer.dart';
 
 import 'package:yc_app_utils/yc_app_utils.dart';
 
@@ -30,6 +31,8 @@ class StyledImageWidget extends StatelessWidget {
           )
         : CachedNetworkImage(
             imageUrl: url,
+            width: double.infinity,
+            height: 200,
             httpHeaders: const {
               "Accept": "image/webp,image/apng,image/*,*/*;q=0.8",
             },
@@ -37,6 +40,13 @@ class StyledImageWidget extends StatelessWidget {
             color: styledImageData.fillColor != null
                 ? CommonHelpers.v2ColorFromHex(styledImageData.fillColor)
                 : null,
+            placeholder: (_, __) => Shimmer.fromColors(
+              baseColor: AppColors.cBLACK_10,
+              highlightColor: AppColors.cWHITE_100,
+              child: Container(
+                color: AppColors.cBLACK_10,
+              ),
+            ),
           );
     return SizedBox(
       height: styledImageData.height,
