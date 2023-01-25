122fd489a5da5bfb98a4abfdbd97dd62785a31de
kshivam1177 <shivam@ivypods.com>
2023-01-25 13:51:32 +0530

    changes of icon sync

--- lib/ui/components/styled_components/styled_video.widget.dart
+++ lib/ui/components/styled_components/styled_video.widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:yc_app_utils/models/click_action/one_click_action.model.dart';
 import 'package:yc_app_utils/models/styled_component/styled_video.model.dart';
 import 'package:yc_app_utils/ui/components/generic_network_image.dart';
+import 'package:yc_app_utils/ui/media_query/yc_media_query.dart';
 import 'package:yc_app_utils/ui/styleguide/colors.dart';
 import 'package:yc_app_utils/ui/styleguide/spacing.dart';
 
@@ -13,7 +15,7 @@ class StyledVideoWidget extends StatefulWidget {
   }) : super(key: key);
 
   final StyledVideoModel styledVideoData;
-  final Widget Function(ValueNotifier<bool>) getVideoPlayer;
+  final Widget Function(ValueNotifier<bool>, bool playInMute) getVideoPlayer;
   final void Function(StyledVideoIconModel) onClick;
 
   @override
@@ -32,14 +34,40 @@ class _StyledVideoWidgetState extends State<StyledVideoWidget> {
     _icons.clear();
     _overlayIcons.clear();
 
-    videoPlayer = widget.getVideoPlayer.call(showImage);
+    bool _playInMute = false;
 
-    widget.styledVideoData.icons?.forEach((element) {
+    for (int x = 0; x < (widget.styledVideoData.icons?.length ?? 0); x++) {
+      final element = widget.styledVideoData.icons![x];
+
+      // for creating a list that align icons accordingly
       if (element.iconPosition != null) {
         _icons.putIfAbsent(element.iconPosition!, () => []);
         _icons[element.iconPosition!]?.add(element);
       }
-    });
+
+      // for setting initial [play]/[pause] value to player and sync with icon
+      final _isPlayPause = element.clickAction?.actions.any((ea) {
+        return ea.functionType == V2FunctionTypesEnum.PLAY_PAUSE;
+      });
+
+      if (_isPlayPause ?? false) {
+        element.isActive = widget.styledVideoData.autoPlay;
+        continue;
+      }
+
+      // for setting initial [mute]/[unmute] value to player and sync with icon
+      final _isMuteUnMute = element.clickAction?.actions.any((ea) {
+        return ea.functionType == V2FunctionTypesEnum.MUTE_UNMUTE;
+      });
+      if (_isMuteUnMute ?? false) {
+        element.isActive = widget.styledVideoData.volume > 0;
+        // widget.videoPlayerBloc.setAutoPlayInMute = !element.isActive;
+        _playInMute = !element.isActive;
+        continue;
+      }
+    }
+
+    videoPlayer = widget.getVideoPlayer.call(showImage, _playInMute);
     _buildIcons();
   }
 
@@ -88,7 +116,7 @@ class _StyledVideoWidgetState extends State<StyledVideoWidget> {
     if (url.isEmpty) {
       return const SizedBox.shrink();
     }
-    double _screenWidth = MediaQuery.of(context).size.width;
+    double _screenWidth = YCMediaQuery.screenWidth!;
     double _width = _screenWidth * widget.styledVideoData.width;
 
     return ClipRect(
