50c283567434a09f4173c422945fe3b214a4e0ad
kshivam1177 <shivam@ivypods.com>
2023-03-16 13:13:04 +0530

    changes of video

--- example/lib/form_screen/form_screen.dart
+++ example/lib/form_screen/form_screen.dart
@@ -21,7 +21,7 @@ class _FormScreenState extends State<FormScreen> {
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: [],
+          children: const [],
         ),
       ),
     );
